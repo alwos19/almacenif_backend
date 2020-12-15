44ca2e5b04451a11ef03b91962c6058488dedf4b
raiden <jhon.jaramilloe@udea.edu.co>
2020-12-15 07:38:50 -0500

    API users

--- src/routes/roles.js
+++ src/routes/roles.js
@@ -4,7 +4,12 @@ const Rol = require('../databases/models/Roles');
 router.get('/roles', async(req, res) => {
     const roles = await Rol.findAll();
     res.json(roles);
-})
+});
+
+router.post('/roles', async(req, res) => {
+    const rol = await Rol.create(req.body);
+    res.json(rol);
+});
 
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,16 @@ const User = require('../databases/models/Users');
 const Rol = require('../databases/models/Roles');
 
 
+
 router.get('/users', async(req, res) => {
-    const user = await User.findAll();
+    const user = await User.findAll({
+        include: {
+            model: Rol,
+            as: "rol",
+            attributes: ['nombre']
+        },
+        attributes: ['nombre', 'cedula']
+    });
     res.json(user);
 })
 
@@ -13,11 +21,19 @@ router.post('/users/signup', async(req, res) => {
     res.json(user);
 });
 
-router.put('/users/:id', async(req, res) => {
-    let id = await req.params.id;
-    res.json({
-        id
+router.put('/users/:userId', async(req, res) => {
+    await User.update(req.body, {
+        where: { id: req.params.userId }
     });
+
+    res.json({ succes: 'se ha modificado el usuario' })
+});
+
+router.delete('/users/:userId', async(req, res) => {
+    await User.destroy({
+        where: { id: req.params.userId }
+    });
+    res.json({ success: 'se ha borrado el usuario' })
 })
 
 module.exports = router;
